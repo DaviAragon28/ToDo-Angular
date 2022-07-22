6ec51062156310d5c2672fb06434c3d7daeb1611
David Aragón <Davidaragon0007@gmail.com>
2022-07-22 16:16:07 -0500

    En desarrollo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  tasks: string[] = ['a']
+  tasks: string[] = []
   task: string = ''
   btnDisabled: boolean = false;
   newTask() {
